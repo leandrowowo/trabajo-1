03312b33d3c8de53292da54c2633b73872d91bb3
Delian Santis Lopez <delian.santis@usach.cl>
2025-09-17 01:15:06 -0300

    Cambie el main a uno mas intuitivo y mejor

--- t1.c
+++ t1.c
@@ -27,12 +27,12 @@ void Usage(char *message) {
   
    printf("\nUsage: %s k -M -O < datafile",message);
    printf("\n\nk: number of threads (k >= 0)");
-   printf("\n\nM in {T,P} \t(T: Secuential, P: Parallel )");
+   printf("\n\nM in {T,R} \t(T: Traditional, R: Russian )");
    printf("\n\nO in {S,V} \t (S: Silence, V: Verbose) \n\n");
 }
 
 void read_data(int *m, int *n, int ***numbers) {
-
+    int i;
     //PONER CONDICION DE QUE SI HAY MÁS DE DOS FILAS, ERROR
     //PONER CONDICION DE QUE SI HAY MÁS DE 2 COLUMNAS, ERROR
     //PONER CONDICION DE QUE SI HAY MÁS NUMEROS QUE LA CANTIDAD DE DIGITOS DE M Y N, ERROR
@@ -42,26 +42,26 @@ void read_data(int *m, int *n, int ***numbers) {
 
     (*numbers)[0] = (int *)calloc(*m, sizeof(int)); // asignamos memoria para la primera fila cantidad M
 
-    for(int i = 0; i < *m; i = i + 1)
+    for(i = 0; i < *m; i = i + 1)
         scanf("%d", &(*numbers)[0][i]); //asignamos los valores a la primera fila
 
     scanf("%d", n); // cantidad de dígitos del segundo número
-
     (*numbers)[1] = (int *)calloc(*n, sizeof(int)); // asignamos memoria para la segunda fila cantidad N
 
-    for(int i = 0; i < *n; i = i + 1)
+    for(i = 0; i < *n; i = i + 1)
         scanf("%d", &(*numbers)[1][i]); //asignamos los valores a la segunda fila
 }
 
 //prueba para imprimir los numeros 
 void imprimir(int m, int n, int **numbers){
+    int i;
     printf("\nPrimer numero: ");
-    for(int i = 0; i < m; i = i + 1)
+    for(i = 0; i < m; i = i + 1)
         printf("%d", numbers[0][i]); //impresion de la fila 0
     printf("\n");
 
     printf("\nSegundo numero: ");
-    for(int i = 0; i < n; i = i + 1)
+    for(i = 0; i < n; i = i + 1)
         printf("%d", numbers[1][i]); // impresion de la fila 1
     printf("\n");
 }
@@ -69,11 +69,13 @@ void imprimir(int m, int n, int **numbers){
 int transformar(int **numbers, int m, int n, int *num1, int *num2){
     *num1 = 0;
     *num2 = 0;
+    int i;
 
-    for(int i = 0; i < m; i = i + 1)
+
+    for(i = 0; i < m; i = i + 1)
         *num1 = *num1*10 + numbers[0][i]; //transformar la fila 0 en un numero entero
 
-    for(int i = 0; i < n; i = i + 1)
+    for(i = 0; i < n; i = i + 1)
         *num2 = *num2*10 + numbers[1][i]; // lo mismo para este pero con la fila 1
 }
 
@@ -115,37 +117,42 @@ int main (int argc, char **argv){
         return 0;
     }
     
-    if(strcmp(argv[2],"-T") == 0 && strcmp(argv[3],"-S") == 0){
+    if(strcmp(argv[2], "-T") == 0){
+        if (strcmp(argv[3], "-S") == 0)
+        {
+            //LEANDRO
+        }
+        else if(strcmp(argv[3], "-V") == 0){
 
-        csc = clock(); // cpu start
-        result = russian_secuential(&a, &b);
-        cec = clock(); // cpu exit
-
-        E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
-
-        printf ("Resultado: %d\n", result);
-        printf("CPU time: %f\n", E_cpu);
+            //LEANDRO
+        }
+        else{
+            Usage(argv[0]);
+            return 0;
+        }
     }
+    else if (strcmp(argv[2], "-R") == 0)
+    {
+        if (strcmp(argv[3], "-S") == 0)
+        {
+            csc = clock(); // cpu start
+            result = russian_secuential(&a, &b);
+            cec = clock(); // cpu exit
 
-    else if(strcmp(argv[2],"-P") == 0 && strcmp(argv[3],"-S") == 0){
+            E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
 
-        //ruso_paralelo();
+            printf ("Resultado: %d\n", result);
+            printf("CPU time: %f\n", E_cpu);
+        }
+        else if(strcmp(argv[3], "-V") == 0){
 
+            //FALTA HACER MODO VERBOSO
+        }
+        else{
+            Usage(argv[0]);
+            return 0;
+        }
     }
-
-    else if(strcmp(argv[2],"-T") == 0 && strcmp(argv[3],"-V") == 0){
-
-        imprimir(m, n, numbers);
-        result = russian_secuential(&a, &b);
-        printf ("\n\nResultado: %d\n", result);
-    }
-
-    else if(strcmp(argv[2],"-P") == 0 && strcmp(argv[3],"-V") == 0){
-
-        imprimir(m, n, numbers); //Funcion distinta para ver que le toca a cada hilo
-        //ruso_paralelo();
-    }
-        
     else{
         Usage(argv[0]);
         return 0;
