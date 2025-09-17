e364d705e8b25222da7ad7d036737b02b1152859
Delian Santis Lopez <delian.santis@usach.cl>
2025-09-17 16:54:15 -0300

    Preversion del código finalizado (SECUENCIAL)

--- t1.c
+++ t1.c
@@ -34,30 +34,34 @@ void Usage(char *message) {
    printf("\n\nO in {S,V} \t (S: Silence, V: Verbose) \n\n");
 }
 
-void read_data(int *m, int *n, int ***numbers) {
-    int i;
-    //PONER CONDICION DE QUE SI HAY MÁS DE DOS FILAS, ERROR
-    //PONER CONDICION DE QUE SI HAY MÁS DE 2 COLUMNAS, ERROR
-    //PONER CONDICION DE QUE SI HAY MÁS NUMEROS QUE LA CANTIDAD DE DIGITOS DE M Y N, ERROR
+void read_data(unsigned int *m, unsigned int *n, unsigned int ***numbers) {
+    unsigned int i;
 
     scanf("%d", m); // cantidad de dígitos del primer número
-    *numbers = (int **)calloc(2, sizeof(int *)); // memoria para dos filas
+    if (*m > 9){
+        exit(EXIT_SUCCESS);
+    }
+    
+    *numbers = (unsigned int **)calloc(2, sizeof(unsigned int *)); // memoria para dos filas
 
-    (*numbers)[0] = (int *)calloc(*m, sizeof(int)); // asignamos memoria para la primera fila cantidad M
+    (*numbers)[0] = (unsigned int *)calloc(*m, sizeof(unsigned int)); // asignamos memoria para la primera fila cantidad M
 
     for(i = 0; i < *m; i = i + 1)
         scanf("%d", &(*numbers)[0][i]); //asignamos los valores a la primera fila
-
+    
     scanf("%d", n); // cantidad de dígitos del segundo número
-    (*numbers)[1] = (int *)calloc(*n, sizeof(int)); // asignamos memoria para la segunda fila cantidad N
+    if (*n > 9){
+        exit(EXIT_SUCCESS);
+    }
+    (*numbers)[1] = (unsigned int *)calloc(*n, sizeof(unsigned int)); // asignamos memoria para la segunda fila cantidad N
 
     for(i = 0; i < *n; i = i + 1)
         scanf("%d", &(*numbers)[1][i]); //asignamos los valores a la segunda fila
 }
 
 //prueba para imprimir los numeros 
-void imprimir(int m, int n, int **numbers){
-    int i;
+void imprimir(unsigned int m, unsigned int n, unsigned int **numbers){
+    unsigned int i;
     printf("\nPrimer numero: ");
     for(i = 0; i < m; i = i + 1)
         printf("%d", numbers[0][i]); //impresion de la fila 0
@@ -69,33 +73,32 @@ void imprimir(int m, int n, int **numbers){
     printf("\n");
 }
 
-int transformar(int **numbers, int m, int n, int *num1, int *num2){
+void transformar(unsigned int **numbers, unsigned int m, unsigned int n, unsigned long long int *num1, unsigned long long int *num2){
     *num1 = 0;
     *num2 = 0;
-    int i;
+    unsigned int i;
 
+    for(i = 0; i < m; i++)
+        *num1 = *num1*10 + numbers[0][i]; // transformar la fila 0 en un número entero
 
-    for(i = 0; i < m; i = i + 1)
-        *num1 = *num1*10 + numbers[0][i]; //transformar la fila 0 en un numero entero
-
-    for(i = 0; i < n; i = i + 1)
-        *num2 = *num2*10 + numbers[1][i]; // lo mismo para este pero con la fila 1
+    for(i = 0; i < n; i++)
+        *num2 = *num2*10 + numbers[1][i]; // transformar la fila 1
 }
 
-int russian_secuential(int *a, int *b, int mode){
+int russian_secuential(unsigned long long int *a, unsigned long long *b, unsigned int mode){
 
-    int multiply =  0;
+    unsigned long long int multiply = 0;
 
     if (mode == 1) {
-        printf("Proceso de datos:\n"); //Solo si es verboso
-        printf("a ------ b\n");
+        printf("Proceso de datos:\n");  // Solo si es verboso
+        printf("a \t b\n");         // Mostrar cada numero
         printf("------------ \n");
     }
 
     while (*a >= 1 && *b > 0) {
 
         if(mode == 1)
-        printf("%d \t %d\n", *a, *b);
+            printf("%llu \t %llu\n", *a, *b);
 
         if (*a % 2 != 0) { // Si a es impar
             multiply = multiply + *b;
@@ -106,6 +109,25 @@ int russian_secuential(int *a, int *b, int mode){
     return multiply;
 }
 
+unsigned long long traditional_mult(unsigned long long int a, unsigned long long int b)
+{
+    unsigned int digit;
+    unsigned long long partial_result;
+    unsigned long long valor = 0;
+    unsigned long long factor = 1; // Equivale a las potencias de 10 al momento de multiplicar y correrse un espacio
+
+    while(b > 0)
+    {
+        digit = b % 10;     // Toma el último dígito del segundo factor
+        partial_result = a * digit;     // 'a' se convierte a unsigned long long para evitar overflow en la multiplicación
+        valor = valor + partial_result * factor;
+        b = b / 10;     // Divide por 10 para tomar el siguiente digito a multiplicar en la próxima iteración
+        factor = factor * 10;
+    }
+
+    return valor;
+}
+
 
  /*
  * 
@@ -114,10 +136,10 @@ int russian_secuential(int *a, int *b, int mode){
 
 int main (int argc, char **argv){
 
-    int m,n, mode; // tamaño del arreglo y el metodo
-    int **numbers; //arreglo bidimensional
-    int a, b; // numeros a multiplicar
-    int result; // resultado de la multiplicacion
+    unsigned int m,n, mode; // tamaño del arreglo y el metodo
+    unsigned int **numbers; //arreglo bidimensional
+    unsigned long long int a, b;        // números a multiplicar
+    unsigned long long int result;       // resultado de la multiplicación
     float E_cpu;
     clock_t csc, cec;
 
@@ -135,11 +157,28 @@ int main (int argc, char **argv){
     if(strcmp(argv[2], "-T") == 0){
         if (strcmp(argv[3], "-S") == 0){
             mode = SILENCE;
-            //LEANDRO
+
+            csc = clock(); // cpu start
+            result = traditional_mult(a , b);
+            cec = clock(); // cpu exit
+
+            E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
+
+            printf("Resultado multiplicación: %llu\n", result);
+            printf("\nCPU time: %f\n\n", E_cpu);
         }
         else if(strcmp(argv[3], "-V") == 0){
             mode = VERBOSE;
-            //LEANDRO
+            
+            csc = clock(); // cpu start
+            result = traditional_mult(a , b);
+            cec = clock(); // cpu exit
+
+            E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
+
+            printf("Resultado multiplicación: %llu\n", result);
+            printf("\nCPU time: %f\n\n", E_cpu);
+
         }
         else{
             Usage(argv[0]);
@@ -157,7 +196,7 @@ int main (int argc, char **argv){
 
             E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
 
-            printf ("\nResultado: %d\n", result);
+            printf ("\nResultado: %llu\n", result);
             printf("\nCPU time: %f\n\n", E_cpu);
         }
 
@@ -169,7 +208,7 @@ int main (int argc, char **argv){
 
             E_cpu = (float)(cec - csc) / CLOCKS_PER_SEC;
 
-            printf ("\nResultado: %d\n", result);
+            printf ("\nResultado: %llu\n", result);
             printf("\nCPU time: %f\n", E_cpu);
         }
         else{
